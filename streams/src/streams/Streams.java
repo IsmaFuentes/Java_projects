0356640ce85855935a21a162a7a5fc759219c977
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-02-17 19:25:24 +0100

    Add files via upload

--- streams/src/streams/Streams.java
+++ streams/src/streams/Streams.java
@@ -36,7 +36,8 @@ public class Streams {
         
         switch(opcion){
             case 1:
-                imprimirAlumnos();
+                //imprimirAlumnos();
+                escribirDesdeFichero();
                 break;
             case 2:
                 escribirDatos();
@@ -50,10 +51,104 @@ public class Streams {
         }
         
     }
+    //Genera un archivo txt con datos de los alumnos que son mayores de edad
+    public static void imprimirAlumnos(){
+        FileOutputStream fos = null;
+        File file;
+        
+        String contenido = "-------------------\r\n"
+                         + "Listado de alumnos \r\n"
+                         + "-------------------\r\n";
+        
+        for(int i = 0; i < misAlumnos.length; i++){
+            if(misAlumnos[i].edad > 17){
+                contenido += "-----Alumno nº"+i+"----\r\n"+
+                             "Nombre: "+misAlumnos[i].getNombre()+"\r\n"+
+                             "Apellido: "+misAlumnos[i].getApellido()+"\r\n"+
+                             "Edad: "+misAlumnos[i].getEdad()+"\r\n";
+            }else{
+                //System.out.println("nada");
+            }
+        }
+        
+        try {
+            file = new File("C:/Users/Ismael/Desktop/ficheros/documento.txt");
+            fos = new FileOutputStream(file);
+            
+            if(!file.exists()){
+                file.createNewFile();//si el fichero no existe creará uno
+            }
+            
+            byte[] arrayDeBytes = contenido.getBytes();
+            fos.write(arrayDeBytes);
+            fos.flush();
+            System.out.println("Datos escritos!!");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        finally{
+            try{
+                if(fos != null){
+                    fos.close();
+                }
+            }catch(Exception e){
+                System.out.println("Error al cerrar el stream");
+            }
+        }
+    }
+    
+    
+   /* Archivo txt con el que trabajamos
+    Pepe García 28
+    Juan Sánchez 18
+    Adrián Hernández 17
+    Santiago García 15
+    */
+    //Lee los datos de un fichero y los imprime en otro aplicandole formato
+    public static void escribirDesdeFichero(){
+        File fichero = new File("C:/Users/Ismael/Desktop/ficheros/entrada.txt");
+        String linea = null;
+        int id = 1;
+        
+        String contenido = "------------------\r\n"
+                         + "Listado de alumnos\r\n"
+                         + "------------------\r\n";
+        
+        String alumnos = "";
+        
+        try{
+            FileReader leerfichero = new FileReader(fichero);
+            BufferedReader miBuffer = new BufferedReader(leerfichero);
+            
+            while((linea = miBuffer.readLine()) != null){
+                //separamos el string del fichero por lineas
+                String[] array = linea.split("\\s+");
+                
+                //Por cada línea comprobamos si existint num = 1;e un número entre 18 y 99
+                //expr regular ^0?(1[89]|[2-9]\d)$
+                
+                for(int i = 0; i < array.length; i++){
+                    //si en la línea se cumple la condición esta se guarda en
+                    //la cadena alumnos
+                    if(array[i].matches("^0?(1[89]|[2-9]\\d)$")){
+                        alumnos+= "----Alumno nº"+id+"----\n"+linea+"\n";
+                        id++;
+                    }
+                }  
+            }
+            //cerramos el buffer
+            miBuffer.close();
+            
+            System.out.println(contenido+alumnos);
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
     
     //método que lee un fichero .txt e imprime su contenido con formato
     public static void leerTxtConFormato(){
-        File fichero = new File("C:/Users/Ismael/Desktop/leer.txt");
+        File fichero = new File("C:/Users/Ismael/Desktop/ficheros/leer.txt");
         String linea = null;
         
         try{
@@ -83,7 +178,7 @@ public class Streams {
     
     //método que lee un fichero .txt e imprime su contenido
     public static void leerTxt(){
-        File fichero = new File("C:/Users/Ismael/Desktop/leer.txt");
+        File fichero = new File("C:/Users/Ismael/Desktop/ficheros/leer.txt");
         String linea = null;
         
         try{
@@ -102,52 +197,6 @@ public class Streams {
 
     }
     
-    //Genera un archivo txt con datos de los alumnos que son mayores de edad
-    public static void imprimirAlumnos(){
-        FileOutputStream fos = null;
-        File file;
-        
-        String contenido = "-------------------\r\n"
-                         + "Listado de alumnos \r\n"
-                         + "-------------------\r\n";
-        
-        for(int i = 0; i < misAlumnos.length; i++){
-            if(misAlumnos[i].edad > 17){
-                contenido += "-----Alumno nº"+i+"----\r\n"+
-                             "Nombre: "+misAlumnos[i].getNombre()+"\r\n"+
-                             "Apellido: "+misAlumnos[i].getApellido()+"\r\n"+
-                             "Edad: "+misAlumnos[i].getEdad()+"\r\n";
-            }else{
-                //System.out.println("nada");
-            }
-        }
-        
-        try {
-            file = new File("C:/Users/Ismael/Desktop/documento.txt");
-            fos = new FileOutputStream(file);
-            
-            if(!file.exists()){
-                file.createNewFile();//si el fichero no existe creará uno
-            }
-            
-            byte[] arrayDeBytes = contenido.getBytes();
-            fos.write(arrayDeBytes);
-            fos.flush();
-            System.out.println("Datos escritos!!");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        finally{
-            try{
-                if(fos != null){
-                    fos.close();
-                }
-            }catch(Exception e){
-                System.out.println("Error al cerrar el stream");
-            }
-        }
-    }
-    
     //método que crea un txt con el mensaje "hola mundo"
     public static void escribirDatos(){
         FileOutputStream fos = null;
@@ -155,7 +204,7 @@ public class Streams {
         String contenido = "Hola mundo";
         
         try {
-            file = new File("C:/Users/Ismael/Desktop/documento.txt");
+            file = new File("C:/Users/Ismael/Desktop/ficheros/documento.txt");
             fos = new FileOutputStream(file);
             
             if(!file.exists()){
