98958d4b31664ef2e7ceabe2c214eee220bd91bc
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-02-20 22:47:31 +0100

    Add files via upload

--- transposicion_matrices/src/transposicion_matrices/Transposicion_matrices.java
+++ transposicion_matrices/src/transposicion_matrices/Transposicion_matrices.java
@@ -6,59 +6,52 @@ import java.util.*;
  */
 public class Transposicion_matrices {
     
-    public static Scanner datos = new Scanner(System.in);
-
+    static Scanner datos = new Scanner(System.in);
+    static int filas;
+    static int columnas;
+    
     public static void main(String[] args) {
         
         System.out.print("introduce filas: ");
-        int m = datos.nextInt();
+        filas = datos.nextInt();
         System.out.print("Introduce columnas: ");
-        int n = datos.nextInt();
+        columnas = datos.nextInt();
         
-        int matriz[][] = new int[m][n];
-        int transpuesta[][] = new int[n][m];
+        //inicializamos la matriz
+        int matriz[][] = new int[filas][columnas];
         
-        System.out.println("Matriz MxN");
-        generaMatriz(matriz);
-        System.out.println();
-        System.out.println("Matriz NxM");
-        transponerMatriz(matriz,transpuesta);
+        //la llenamos con valores aleatorios 
+        llenarMatriz(matriz);
+        System.out.println("");
+        //se realiza su transposición
+        transponer(matriz);
+  
     }
     
+    
     //Método que imprime la primera matriz 
-    public static void generaMatriz(int matriz[][]){
+    public static int[][] llenarMatriz(int matriz[][]){
+        System.out.println("Matriz");
         for(int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz[i].length; j++){
-                //los números de la matriz se generan aleatoriamente
                 matriz[i][j] = (int)(Math.random()*9+1);
                 System.out.print("["+matriz[i][j]+"]");
             }
         System.out.println("");
-        }   
+        } 
+        return matriz;
     }
     
-    //Método que realiza la transposición de la matriz
-    public static void transponerMatriz(int matriz[][],int transpuesta[][]){
+    //método que recibe una matriz por parámetro y realiza una transposición
+    public static int[][] transponer(int matriz[][]){
+        int transpuesta[][] = new int[columnas][filas];
+        System.out.println("Matriz transpuesta");
         for(int i = 0; i < transpuesta.length; i++){
             for(int j = 0; j < transpuesta[i].length; j++){
                 System.out.print("["+matriz[j][i]+"]");
             }
-        System.out.println("");
-        }   
+            System.out.println("");
+        }
+        return transpuesta;
     }
 }
-
-
-        //int matriz[][] = new int[4][2];
-        /*
-        matriz[0][0] = 2;
-        matriz[0][1] = 4;
-
-        matriz[1][0] = 6;
-        matriz[1][1] = 6;
-
-        matriz[2][0] = 8;
-        matriz[2][1] = 5;
-        
-        matriz[3][0] = 2;
-        matriz[3][1] = 1;*/
