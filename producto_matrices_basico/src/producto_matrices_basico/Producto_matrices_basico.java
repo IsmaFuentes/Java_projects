90b83510f42a9f1caf55448f50996549ec1bc389
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-02-21 12:11:32 +0100

    Add files via upload

---
+++ producto_matrices_basico/src/producto_matrices_basico/Producto_matrices_basico.java
@@ -0,0 +1,46 @@
+package producto_matrices_basico;
+
+/**
+ *
+ * @author Ismael
+ */
+public class Producto_matrices_basico {
+
+    public static void main(String[] args) {
+        
+        int [][] matriz1 = {{1,3,2},{4,3,1}};
+        int [][] matriz2 = {{3,1},{2,1},{1,2}};
+
+        imprimirResultado(multiplicar(matriz1,matriz2));
+    }
+   
+    //método que suma dos matrices y devuelve el resultado
+    public static int[][] multiplicar(int a[][], int b[][]){
+        int producto[][] = new int[a.length][b[0].length];
+        //comprobamos que pueden multiplicarse
+        if(a[0].length == b.length){
+            //multiplicación de las matrices
+            for(int i = 0; i < producto.length; i++){
+                for(int j = 0; j < producto[i].length; j++){
+                    for(int z = 0; z < a[0].length; z++){
+                        producto[i][j] += a[i][z] * b[z][j];
+                    }
+                }
+            }
+        }else{
+            System.out.println("las matrices no pueden multiplicarse");
+        }
+        return producto;
+    }
+    
+    //método que imprime la matriz
+    public static void imprimirResultado(int resultado[][]){
+        System.out.println("Resultado de la multiplicación");
+        for(int i = 0; i < resultado.length; i++){
+            for(int j = 0; j < resultado[i].length; j++){
+                System.out.print("["+resultado[i][j]+"]");
+            }
+            System.out.println();
+        }       
+    }   
+}
---
+++ suma_matrices/src/suma_matrices/Suma_matrices.java
@@ -0,0 +1,44 @@
+package suma_matrices;
+
+/**
+ *
+ * @author Ismael
+ */
+public class Suma_matrices {
+
+    
+    public static void main(String[] args) {
+        
+        int [][] matriz1 = {{2,4,4},{6,6,9}};
+        int [][] matriz2 = {{2,4,4},{6,6,9}};
+
+        imprimirResultado(sumarMatrices(matriz1,matriz2));
+    }
+   
+    //método que multiplica dos matrices y devuelve el resultado
+    public static int[][] sumarMatrices(int a[][], int b[][]){
+        int suma[][] = new int[a.length][a[0].length];
+        //comprobamos que pueden sumarse
+        if((a.length == b.length) && (a[0].length == b[0].length)){
+            for(int i = 0; i < a.length; i++){
+                for(int j = 0; j < a[i].length; j++){
+                    suma[i][j] = a[i][j] + b[i][j];
+                }
+            } 
+        }else{
+            System.out.println("las matrices no pueden sumarse");
+        }
+        return suma;
+    }
+    
+    //método que imprime la matriz
+    public static void imprimirResultado(int resultado[][]){
+        System.out.println("Resultado de la suma");
+        for(int i = 0; i < resultado.length; i++){
+            for(int j = 0; j < resultado[i].length; j++){
+                System.out.print("["+resultado[i][j]+"]");
+            }
+            System.out.println();
+        }       
+    }
+}
